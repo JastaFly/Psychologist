c2f769dc7395e347330b8a472f02c7b0ff61ef1e
Влад Мелёшкин <zumawuu@gmail.com>
2025-10-26 21:04:41 +0300

    Refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
-const { animate } = anime
+const {animate} = anime
 const showModalButtons = document.querySelectorAll('[data-show-modal]')
 const closeModal = document.querySelector('.modal-form__close')
 const burgerMenu = document.querySelector('.bar')
 const closeMobileMenu = document.querySelector('.menu-mobile__icon')
 
 function modalToggle(elem, displayValue) {
-
-
     elem.style.display = displayValue
 }
 
@@ -14,13 +12,12 @@ for (let button of showModalButtons) {
     button.addEventListener('click', (event) => {
         const modal = document.querySelector('.modal')
         modalToggle(modal, 'block')
-
-
     })
 }
 
 closeModal.addEventListener('click', (event) => {
     const modal = document.querySelector('.modal')
+
     modalToggle(modal, 'none')
 })
 
@@ -31,8 +28,7 @@ burgerMenu.addEventListener('click', () => {
         display: 'block',
         height: [0, 100],
         duration: 500,
-        begin: function(anim) {
-
+        begin: function (anim) {
             mobileMenu.style.display = 'block';
         }
     })
@@ -43,8 +39,7 @@ closeMobileMenu.addEventListener('click', () => {
     animate(mobileMenu, {
         height: [100, 0],
         duration: 500,
-        complete: function(anim) {
-
+        complete: function (anim) {
             setTimeout(() => {
                 mobileMenu.style.display = 'none';
             }, 500)
